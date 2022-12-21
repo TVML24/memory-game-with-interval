44a1cebe7351c83af8fe6aa2df0b00d88dce6ae0
Tim <tvanmookletcher@gmail.com>
2022-12-21 17:08:11 +1030

    initial commit, basic styling and JS pseudocoding

---
+++ assets/script.js
@@ -0,0 +1,25 @@
+// variables for each card
+
+// random number generator for each card that sets the data attribute for each card variable when start is pressed
+//BUT ensures that there MUST be at least 2 card variables with matching values
+//data-state on the cards changes to hidden
+
+// a timer starts counting down from thirty seconds
+
+// When event listener hears a click on a card
+
+// the data-state changes from hidden to visible displaying the stored variable made by the random number generator
+
+//this variable is then stored in a new variable
+
+//when the second card is clicked it compares the variable from the first card to the second card
+
+//if they match both cards change background colour so that they are no longer visible
+
+//if timer reaches zero before the player has won the game header displays "You Lose" and playerLoss++
+
+//if all the cards are now the background colour header displays "You Win" and playerWin++
+
+//clear function activates at the end to return cards to their original state
+
+//write score function writes the score in a text area in the footer
